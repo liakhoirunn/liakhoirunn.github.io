099d38c0e51dba536937a12e804b207ac1d48798
liakhoirunn <liakhoriunnisa707@gmail.com>
2024-01-08 14:26:21 +0700

    commit1

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["student S1 Informatics", "UI/UX Designer", "Programmer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true 
+})
